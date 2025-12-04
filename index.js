5ef9beb487ab59102b7697a0fc58ff056eca29f8
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-04 19:35:36 +0530

    DSA in javascript

---
+++ index.js
@@ -0,0 +1,68 @@
+// let arr = [10, 5, 2, 7, 3];
+
+// function searchElement(arr, n) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == n) {
+//             return i;
+//         }
+        
+//     }
+//     return -1;
+// }
+
+// console.log(searchElement(arr, 5));
+// console.log(searchElement(arr, 10));
+// console.log(searchElement(arr, 3));
+// console.log(searchElement(arr, 11));
+
+
+// Count a negative nUmber
+//  let arr= [4,-8,2,-3,6,-11,10];
+
+//  function countNegative(arr){
+//     let  count= 0;
+//     for(let i =0; i < arr.length ; i++){
+//         if(arr[i] < 0){
+//             count++;
+//         } 
+//     }
+//     return count;
+//  }
+//  console.log(countNegative(arr));
+
+
+// Find the largest number inarray
+// let arr= [-4,-8,-2,-3,-6,-11,-10];
+// function largestNumber(arr){
+//     let largNum = - Infinity;
+//     for(let i =0;i < arr.length;i++){
+//         if (arr[i] > largNum) {
+//             largNum = arr[i];
+//         }
+//     }
+//     return `This is largest ${largNum}` //2
+// }
+
+// console.log(largestNumber(arr));
+
+//find the Second largest
+let arr1= [1,,2,9,5,10,13,13];
+function seclargestNumber(arr1){
+    if (arr1.length < 2) {
+        return null;
+    }
+    let largNum = - Infinity;
+    let sencondlargNum = - Infinity;
+    for(let i =0;i < arr1.length;i++){
+        if (arr1[i] > largNum) {
+            sencondlargNum = largNum;
+            largNum = arr1[i];
+        }
+        else if(arr1[i] > sencondlargNum && arr1[i] != largNum){
+            sencondlargNum = arr1[i];
+        }
+    }
+    return `This is Second largest ${sencondlargNum}` //10
+}
+
+console.log(seclargestNumber(arr1));
---
+++ loops.js
@@ -0,0 +1,8 @@
+// LOOp under a lOOp
+for(let i = 0;i<3;i++){
+    for(let j =0;j<i;j++){
+        console.log("i="+i+" j="+j);
+        
+    }
+}
+
