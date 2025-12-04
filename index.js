6695c116cec00462ecc2975d713d7ba1dabb5aba
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-04 21:32:08 +0530

    todays

--- index.js
+++ index.js
@@ -46,23 +46,58 @@
 // console.log(largestNumber(arr));
 
 //find the Second largest
-let arr1= [1,,2,9,5,10,13,13];
-function seclargestNumber(arr1){
-    if (arr1.length < 2) {
-        return null;
-    }
-    let largNum = - Infinity;
-    let sencondlargNum = - Infinity;
-    for(let i =0;i < arr1.length;i++){
-        if (arr1[i] > largNum) {
-            sencondlargNum = largNum;
-            largNum = arr1[i];
-        }
-        else if(arr1[i] > sencondlargNum && arr1[i] != largNum){
-            sencondlargNum = arr1[i];
-        }
-    }
-    return `This is Second largest ${sencondlargNum}` //10
-}
+// let arr1= [1,,2,9,5,10,13,13];
+// function seclargestNumber(arr1){
+//     if (arr1.length < 2) {
+//         return null;
+//     }
+//     let largNum = - Infinity;
+//     let sencondlargNum = - Infinity;
+//     for(let i =0;i < arr1.length;i++){
+//         if (arr1[i] > largNum) {
+//             sencondlargNum = largNum;
+//             largNum = arr1[i];
+//         }
+//         else if(arr1[i] > sencondlargNum && arr1[i] != largNum){
+//             sencondlargNum = arr1[i];
+//         }
+//     }
+//     return `This is Second largest ${sencondlargNum}` //10
+// }
 
-console.log(seclargestNumber(arr1));
+// console.log(seclargestNumber(arr1));
+
+
+// Print a star pattrens
+
+//#1.........................................
+// let n = 5;
+// for(let i = 1;i<=n;i++ ){
+//     let row="";
+//     for(let j=1;j<=i;j++){
+//         row = row +" *";
+//     }
+//     console.log(row);
+// } 
+
+//#2................................................................
+// let n = 5;
+// for(let i = 1;i<=n;i++ ){
+//     let row="";
+//     for(let j=1;j<=i;j++){
+//         row = row +" "+j;
+//     }
+//     console.log(row);
+// } 
+
+
+//#3.......................................................................
+
+ let n = 5;
+for(let i = 1;i<=n;i++ ){
+    let row="";
+    for(let j=1;j<=i;j++){
+        row = row +" "+i;
+    }
+    console.log(row);
+} 
