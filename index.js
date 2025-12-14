1068aaef8ce024e11413ec1ac7cf60522a7fc08a
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-14 11:55:20 +0530

    Reverse String

--- index.js
+++ index.js
@@ -296,3 +296,19 @@
 //     }
 //     return x
 // };
+
+//Reverse String
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+// var reverseString = function(s) {
+//     let len = s.length;
+//     let halfLen = Math.floor(len/2);
+
+//     for(let i =0 ; i < halfLen ; i++){
+//         let temp = s[i];
+//         s[i] = s[len-1-i];
+//         s[len-1-i] = temp;
+//     }
+// };
