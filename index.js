4cba74d6362ea05f0ad46ce2bc9fa44625911857
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-20 23:33:36 +0530

    485. Max Consecutive Ones

--- index.js
+++ index.js
@@ -337,3 +337,21 @@
 //         }
 //     }
 // };
+//485. Max Consecutive Ones
+// var findMaxConsecutiveOnes = function(nums) {
+//     let maxCount = 0;
+//     let currentCount = 0;
+    
+//     for(let i = 0; i < nums.length; i++) {
+//         if(nums[i] === 1) {
+//             currentCount++;
+//             if(currentCount > maxCount) {
+//                 maxCount = currentCount;
+//             }
+//         } else {
+//             currentCount = 0;
+//         }
+//     }
+    
+//     return maxCount;
+// };
