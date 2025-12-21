6a367d2c6633dad34287094e2f575fa4b2af5dff
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-21 23:42:26 +0530

    268. Missing Number

--- index.js
+++ index.js
@@ -355,3 +355,14 @@
     
 //     return maxCount;
 // };
+
+// 268. Missing Number
+// var missingNumber = function(nums) {
+//     let n = nums.length;
+//     let totalSum = (n*(n+1))/2;
+//     let partialSum = 0;
+//     for(let i=0; i < nums.length; i++){
+//         partialSum = partialSum + nums[i];
+//     }
+//     return totalSum - partialSum;
+// };
