9af07c8836e0c5ffb8fc72e4eda4f2466c712fa6
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-30 11:52:33 +0530

    Bubble Sort in JS

--- index.js
+++ index.js
@@ -466,3 +466,28 @@
 //      }
 //      return -1;
 // };
+
+//Bubble Sort in JS
+
+// let arr = [9,5,3,1,2,10];
+// let n = arr.length;
+// function bubbleSort(arr) {
+//     for(let i = 0; i < n - 1; i++){
+//         let isSorted = false;
+//      for(let j = 0; j < n - 1 - i; j++){
+//         if(arr[j] > arr[j+1]){
+//            let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//             isSorted =  true;
+//         }
+//      }
+//      if(!isSorted){
+//         break;
+//      }
+// }
+// return arr;
+// }
+
+// let result = bubbleSort(arr);
+// console.log(result);
